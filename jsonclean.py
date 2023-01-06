c7aea182dd1e64adcdd454914cdb26155bc06238
Gekk0 <nolancraze@gmail.com>
2023-01-06 16:57:01 -0600

    more code work

--- jsonclean.py
+++ jsonclean.py
@@ -2,25 +2,25 @@ import pandas as pd
 
 def cleanrawjson(sheet : pd.DataFrame):
 
-    #this works under an assumption of normality
+    #this works under an assumption of normality between all sheets using the current settings
     firstRowGone = False
     secondRowGone = False
     
-    for data in sheet:
+    for row in sheet:
 
         if secondRowGone == False and firstRowGone == True:
-            print("second row removing " + data)
-            sheet.pop(data)   
+            #print("second row removing " + data)
+            sheet.pop(row)   
             secondRowGone = True
         if firstRowGone == False:
-            print("first row removing " + data)
-            sheet.pop(data)
+            #print("first row removing " + data)
+            sheet.pop(row)
             firstRowGone = True
     
     i = 1
     for data in sheet:
         if i % 3 == 0:
-            sheet.pop(data)
+            sheet.pop(row)
         i = i + 1
         
     return(sheet)
